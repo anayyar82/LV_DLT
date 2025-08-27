6d817f45508dd3a18ed0a26dbf3fc1157908016f
anayyar82 <anayyar82>
2025-08-27 15:54:18 +0000

    added all new files

---
+++ transformations/profiles_bronze.py
@@ -0,0 +1,35 @@
+import dlt
+from pyspark.sql.functions import col, current_timestamp
+from utilities import utils
+
+redpanda_config = utils.get_redpanda_config(spark = spark, dbutils = dbutils)
+
+@dlt.table(
+    name = "profiles_bronze"
+    ,table_properties={
+        'quality' : 'bronze'
+        ,'delta.enableChangeDataFeed' : 'true'
+        ,'delta.enableDeletionVectors' : 'true'
+        ,'delta.enableRowTracking' : 'true'
+    }
+)
+def profiles_bronze():
+    """
+    Read Stream from the Redpanda Enterprise Quickstart "logins" topic. 
+    """
+    return (
+        spark.readStream
+        .format("kafka")
+        .option("kafka.bootstrap.servers",redpanda_config.get("bootstrap.servers"))
+        .option("subscribe", "profiles")
+        .option("kafka.sasl.mechanism", redpanda_config.get("sasl.mechanism"))
+        .option("kafka.security.protocol", redpanda_config.get("security.protocol"))
+        .option("kafka.sasl.jaas.config", redpanda_config.get("sasl.jaas.config"))
+        # Optional: Set failOnDataLoss to false to avoid query failure if data is missing
+        .option("failOnDataLoss", "false")
+        # Optional: Set startingOffsets to earliest for initial testing
+        .option("startingOffsets", "earliest")
+        .load()
+        .withColumn("value_str", col("value").cast("string"))
+        .withColumn("ingestTime", current_timestamp())
+    )
