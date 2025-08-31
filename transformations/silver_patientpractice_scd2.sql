d27f586dc57d3bd00b23f06c2fbee9864897fb6b
anayyar82 <anayyar82>
2025-08-31 20:00:36 +0000

    dafdsafds

--- transformations/silver_patientpractice_scd2.sql
+++ transformations/silver_patientpractice_scd2.sql
@@ -40,26 +40,26 @@ TBLPROPERTIES (
   'quality' = 'silver'
 );
 
--- ====================================================
--- CDC Flow
--- ====================================================
 CREATE FLOW silver_patientpractice_cdc_scd2 AS AUTO CDC INTO
   silver_patientpractice_scd2
 FROM (
   WITH parsed AS (
     SELECT
       PatientID,
-      PracticeID,
       Shard,
+      PracticeID,
       Created,
       CreatedBy,
       Updated,
       UpdatedBy,
+      P,
+      V,
+      ingestTime,
       _change_type,
       _commit_version,
       _commit_timestamp,
 
-      -- SAFE triple-encoded JSON parsing with PERMISSIVE mode
+      -- SAFE PERMISSIVE JSON parsing of D column as STRUCT
       from_json(
         D,
         'STRUCT<
@@ -82,35 +82,37 @@ FROM (
           updatedBy: STRING
         >',
         map('mode','PERMISSIVE')
-      ) AS variant_col
+      ) AS D_variant
     FROM STREAM(bronze_patientpractice_cdf)
   )
   SELECT
     PatientID,
-    PracticeID,
     Shard,
+    PracticeID,
     Created,
     CreatedBy,
     Updated,
     UpdatedBy,
-    -- Flatten JSON fields
-    variant_col:practiceId::string   AS D_practiceId,
-    variant_col:patientId::string    AS D_patientId,
-    variant_col:referrerId::string   AS D_referrerId,
-    variant_col:name::string         AS D_name,
-    variant_col:address1::string     AS D_address1,
-    variant_col:address2::string     AS D_address2,
-    variant_col:city::string         AS D_city,
-    variant_col:state::string        AS D_state,
-    variant_col:zipCode::string      AS D_zipCode,
-    variant_col:country::string      AS D_country,
-    variant_col:phoneNumber::string  AS D_phoneNumber,
-    variant_col:businessId::string   AS D_businessId,
-    variant_col:anonymous::boolean   AS D_anonymous,
-    to_timestamp(variant_col:created::bigint)   AS D_created,
-    variant_col:createdBy::string    AS D_createdBy,
-    to_timestamp(variant_col:updated::bigint)   AS D_updated,
-    variant_col:updatedBy::string    AS D_updatedBy,
+
+    -- Flatten JSON STRUCT fields
+    D_variant:practiceId::string   AS D_practiceId,
+    D_variant:patientId::string    AS D_patientId,
+    D_variant:referrerId::string   AS D_referrerId,
+    D_variant:name::string         AS D_name,
+    D_variant:address1::string     AS D_address1,
+    D_variant:address2::string     AS D_address2,
+    D_variant:city::string         AS D_city,
+    D_variant:state::string        AS D_state,
+    D_variant:zipCode::string      AS D_zipCode,
+    D_variant:country::string      AS D_country,
+    D_variant:phoneNumber::string  AS D_phoneNumber,
+    D_variant:businessId::string   AS D_businessId,
+    D_variant:anonymous::boolean   AS D_anonymous,
+    to_timestamp(D_variant:created::bigint)   AS D_created,
+    D_variant:createdBy::string    AS D_createdBy,
+    to_timestamp(D_variant:updated::bigint)   AS D_updated,
+    D_variant:updatedBy::string    AS D_updatedBy,
+
     current_timestamp() AS processedTime,
     _change_type,
     _commit_version,
@@ -126,7 +128,3 @@ COLUMNS * EXCEPT
   (_change_type, _commit_version, _commit_timestamp)
 STORED AS
   SCD TYPE 2;
-
-
-  Cannot resolve "semi_structured_extract_json_multi(variant_col, $.practiceId)" due to data type mismatch: The first parameter requires the "STRING" type, however "variant_col" has the type "STRUCT<practiceId: STRING, patientId: STRING, referrerId: STRING, name: STRING, address1: STRING, address2: STRING, city: STRING, state: STRING, zipCode: STRING, country: STRING, phoneNumber: STRING, businessId: STRING, anonymous: BOOLEAN, created: BIGINT, createdBy: STRING, updated: BIGINT, updatedBy: STRING>".
-
