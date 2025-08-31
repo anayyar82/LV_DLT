b832f3c0cecf85e29770a7bb6750ff568d1f4845
anayyar82 <anayyar82>
2025-08-31 20:42:40 +0000

    adfsf
    adsfds

--- transformations/silver_patientpractice_scd2.sql
+++ transformations/silver_patientpractice_scd2.sql
@@ -59,15 +59,8 @@ FROM (
       _commit_version,
       _commit_timestamp,
 
-      -- Parse JSON string safely into STRUCT
-      from_json(
-        regexp_replace(
-          regexp_replace(
-            substring(D, 2, length(D)-2),
-            '""', '"'
-          ),
-          '\\\\"', '"'
-        ),
+      -- Safely parse JSON string into STRUCT
+      from_json(D, 
         'STRUCT<
           practiceId: STRING,
           patientId: STRING,
@@ -99,24 +92,26 @@ FROM (
     CreatedBy,
     Updated,
     UpdatedBy,
+
     -- Flatten STRUCT fields
-    D_struct:practiceId::STRING   AS D_practiceId,
-    D_struct:patientId::STRING    AS D_patientId,
-    D_struct:referrerId::STRING   AS D_referrerId,
-    D_struct:name::STRING         AS D_name,
-    D_struct:address1::STRING     AS D_address1,
-    D_struct:address2::STRING     AS D_address2,
-    D_struct:city::STRING         AS D_city,
-    D_struct:state::STRING        AS D_state,
-    D_struct:zipCode::STRING      AS D_zipCode,
-    D_struct:country::STRING      AS D_country,
-    D_struct:phoneNumber::STRING  AS D_phoneNumber,
-    D_struct:businessId::STRING   AS D_businessId,
-    D_struct:anonymous::BOOLEAN   AS D_anonymous,
-    to_timestamp(D_struct:created::BIGINT) AS D_created,
-    D_struct:createdBy::STRING    AS D_createdBy,
-    to_timestamp(D_struct:updated::BIGINT) AS D_updated,
-    D_struct:updatedBy::STRING    AS D_updatedBy,
+    D_struct:practiceId        AS D_practiceId,
+    D_struct:patientId         AS D_patientId,
+    D_struct:referrerId        AS D_referrerId,
+    D_struct:name              AS D_name,
+    D_struct:address1          AS D_address1,
+    D_struct:address2          AS D_address2,
+    D_struct:city              AS D_city,
+    D_struct:state             AS D_state,
+    D_struct:zipCode           AS D_zipCode,
+    D_struct:country           AS D_country,
+    D_struct:phoneNumber       AS D_phoneNumber,
+    D_struct:businessId        AS D_businessId,
+    D_struct:anonymous         AS D_anonymous,
+    to_timestamp(D_struct:created) AS D_created,
+    D_struct:createdBy         AS D_createdBy,
+    to_timestamp(D_struct:updated) AS D_updated,
+    D_struct:updatedBy         AS D_updatedBy,
+
     current_timestamp() AS processedTime,
     _change_type,
     _commit_version,
@@ -132,42 +127,3 @@ COLUMNS * EXCEPT
   (_change_type, _commit_version, _commit_timestamp)
 STORED AS
   SCD TYPE 2;
-
-
-
-
-PatientID
-string
-Shard
-string
-PracticeID
-string
-Created
-string
-CreatedBy
-string
-Updated
-string
-UpdatedBy
-string
-V
-string
-D
-string
-P
-string
-inputFilename
-string
-fullFilePath
-string
-fileMetadata: {"file_path": "string", "file_name": "string", "file_size": "bigint", "file_block_start": "bigint", "file_block_length": "bigint", "file_modification_time": "timestamp"}
-struct
-bronze_prefix
-string
-ingestTime
-timestamp
-ingestDate
-date
-
-
-{"address1":"010 Dicki Union","address2":"22025 Marlin Light","anonymous":false,"businessId":"idxdlfxc71","city":"North Nicolas","country":"US","created":1587214191,"createdBy":"17fdc071-8173-11ea-97b7-0242ac110008","name":"practicevjalt6k7","patientId":"17fdc071-8173-11ea-97b7-0242ac110008","phoneNumber":"01653453370","practiceId":"171ecdf0-8173-11ea-97b7-0242ac110008","referrerId":"13f70c75-8173-11ea-844f-0242ac11000b","shard":1836,"state":"Iowa","updated":1587214191,"updatedBy":"17fdc071-8173-11ea-97b7-0242ac110008","zipCode":"02665"}
